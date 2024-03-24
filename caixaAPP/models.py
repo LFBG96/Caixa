e41e78a78bd2aeacda8e4fb9c327bf62b48caef6
lfbg96 <lf5154223@gmail.com>
2024-03-24 15:38:30 -0300

    Att

--- caixaAPP/models.py
+++ caixaAPP/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Produto(models.Model):
+    nome = models.CharField(max_length=100)
+    valor = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.nome
+
+
+class ItensVenda(models.Model):
+    produto = models.ForeignKey(Produto,on_delete=models.PROTECT)
+    
+    def __str__(self):
+        return self.produto.nome
+
+
+class Carrinho(models.Model):
+    produtos = models.ManyToManyField(to=Produto)
+    total = models.DecimalField(max_digits=10, decimal_places=2)
